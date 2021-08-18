2e35e826a9194865db7a403283ea6a55b0782c32
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2021-08-18 08:26:45 +0800

    Wew

--- database/migrations/2021_08_17_063343_create_researches_table.php
+++ database/migrations/2021_08_17_063343_create_researches_table.php
@@ -17,7 +17,7 @@ class CreateResearchesTable extends Migration
             $table->engine = 'InnoDB';
             $table->smallIncrements('id');
             $table->string('title',200);
-            $table->longText('content')->default('Aa');
+            $table->longText('content');
             $table->smallInteger('iprstatus_id')->unsigned()->index();
             $table->foreign('iprstatus_id')->references('id')->on('dropdowns')->onDelete('cascade');
             $table->smallInteger('classification_id')->unsigned()->index();
